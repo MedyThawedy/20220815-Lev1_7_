92147a2acb78b82e54223e187eedd54e60610796
MD <dhmedy@googlemail.com>
2022-08-15 14:08:21 +0200

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,11 @@
+/*
+Einen Loop mit do...while.
+Es sollen die Nummern von 1 bis 5 im Dokument ausgeben werden.
+*/
+
+let i = 0;
+
+do {
+    i++;
+    console.log('The number is :', i);
+} while (i < 5);
